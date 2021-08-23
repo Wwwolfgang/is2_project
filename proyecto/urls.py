71d9e2464529dad6584471ac7c8c907fc67adfd9
FonecaRod <robertorve27@fpuna.edu.py>
2021-08-23 19:19:51 -0400

    Nuevas urls

--- proyecto/urls.py
+++ proyecto/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth.views import LogoutView
 from proyecto.views import EliminarRolProyecto, agregar_rol_proyecto_view, lista_rol_proyecto_view
 
 urlpatterns = [
-    path('addProjRol/',agregar_rol_proyecto_view),
-    path('listaProjRol/',lista_rol_proyecto_view),
+    path('rol/agregar',agregar_rol_proyecto_view),
+    path('rol/listar',lista_rol_proyecto_view),
     #path('<int:pk>/delProjRol/',EliminarRolProyecto.as_view(), name='rolproyecto-eliminar')
 ]
--- proyecto/views.py
+++ proyecto/views.py
@@ -33,7 +33,7 @@ def agregar_rol_proyecto_view(request):
         if form.is_valid():
             print(form.cleaned_data)
             form.save()
-            return redirect('/proyecto/listaProjRol/')  
+            return redirect('/proyecto/rol/listar')  
         contexto['form'] = form
         return render(request, 'proyecto/nuevo_rol_proyecto_view.html', context=contexto)
     else:
