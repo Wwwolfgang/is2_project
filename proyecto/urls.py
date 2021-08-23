a257cb18100ecd1f381de9076de4b5fb27d3c47c
lezcano17 <jlezcano17@outlook.com>
2021-08-23 19:50:55 -0400

    CRUD proyecto -agregar

--- proyecto/urls.py
+++ proyecto/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from django.views.generic import TemplateView
-
+from .views import ProyectoDetailView, edit, create, delete, IndexView
 
 app_name = 'proyecto'
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name="proyecto/index.html")),
-    
+    path('', TemplateView.as_view(template_name="proyecto/index.html"),name='home'),
+    path('proyecto/', IndexView.as_view(), name='index'),
+    path('proyecto/<int:pk>/', ProyectoDetailView, name='detail'),
+    path('proyecto/edit/<int:pk>/', edit, name='edit'),
+    path('proyecto/create/', create, name='create'),
+    path('proyecto/delete/<int:pk>/', delete, name='delete'),
 ]
--- proyecto/views.py
+++ proyecto/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Proyecto
+from .forms import ProyectoForm
+from django.http import HttpResponseRedirect
+from django.views.generic import ListView, DetailView
+from django.urls import reverse
 
-# Create your views here.
+
+class IndexView(ListView):
+    model = Proyecto
+    template_name = 'proyecto/index.html'
+    context_object_name = 'proyecto_list'
+    queryset = Proyecto.objects.all()
+
+    def get_queryset(self):
+        return Proyecto.objects.all()
+
+
+class ProyectoDetailView(DetailView):
+    model = Proyecto
+    template_name = 'proyecto/proyecto-detalle.html'
+
+
+def create(request):
+    if request.method == 'POST':
+        form = ProyectoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/home')
+            #return HttpResponseRedirect(reverse('proyecto:index'))
+    form = ProyectoForm()
+
+    return render(request,'proyecto/create.html',{'form': form})
+
+def edit(request, pk, template_name='proyecto/edit.html'):
+    proyecto = get_object_or_404(Proyecto, pk=pk)
+    form = ProyectoForm(request.POST or None, instance=post)
+    if form.is_valid():
+        form.save()
+        return redirect('/home')
+    return render(request, template_name, {'form':form})
+
+def delete(request, pk, template_name='proyecto/confirm_delete.html'):
+    proyecto = get_object_or_404(Proyecto, pk=pk)
+    if request.method=='POST':
+        Proyecto.delete()
+        return redirect('/home')
+    return render(request, template_name, {'object':proyecto})
