60d26d3a43b5b04553c1fbae6a7f67d0ab462017
FonecaRod <robertorve27@fpuna.edu.py>
2021-09-02 15:57:30 -0400

    Documentación de forms, models y views

--- proyecto/models.py
+++ proyecto/models.py
@@ -4,6 +4,11 @@ from django.utils import timezone
 from sso.models import User
 # Create your models here.
 class RolProyecto(models.Model):
+    """
+    Model del rol de proyecto.
+    Se almacena el nombre del rol, sus permisos, los participantes del proyecto que tienen el rol,
+    y el proyecto al que pertenece dicho rol.
+    """
     nombre = models.CharField(verbose_name='Nombre del rol', max_length=60, blank=False,null=False)
     permisos = models.ManyToManyField(Permission)
     participantes = models.ManyToManyField(User,blank=True,null=True)
@@ -12,7 +17,11 @@ class RolProyecto(models.Model):
         permissions = (
                     ("p_administrar_roles","Permite que el usuario pueda configurar, crear, importar y eliminar roles del proyecto. Solo los permisos del scrum master no se podrán modificar."),
         )
+
     def get_permisos(self):
+        """
+        Función que retorna la lista de permisos asociados al rol
+        """
         return [p for p in self.permisos.all()]
     
     def __str__(self):
