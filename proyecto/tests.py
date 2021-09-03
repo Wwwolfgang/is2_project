6cc1f6c6deda62a1ecb845bcea7cbcefdc7ba718
FonecaRod <robertorve27@fpuna.edu.py>
2021-09-03 10:36:42 -0400

    Nuevos tests

--- proyecto/tests.py
+++ proyecto/tests.py
@@ -1,26 +1,26 @@
-from typing import Reversible
-import pytest
 from proyecto.models import RolProyecto
-from django.contrib.auth.models import Permission
-from http import HTTPStatus
+from django.db.models import fields
+from django.db.models.query_utils import PathInfo
+from django.http import response
+from django.test import TestCase
 from sso.models import User
-from django.test import Client
-import unittest
-
+from django.urls import reverse
+from django.contrib.auth.models import Group,Permission, User
+from django.test.client import Client, RequestFactory
+from allauth.utils import get_user_model
+import pytest
+from pytest_django.asserts import assertTemplateUsed
 
 # Create your tests here.
 
 
-@pytest.fixture
-def usuario():
-    user = User(is_administrator=True)
-    user.save()
-    return user
+
 @pytest.mark.django_db
 class TestModelRolProyecto:
     """
     Pruebas unitarias que comprueban las funciones del model RolProyecto
     """
+
     def test_rolproyecto_lista_permisos(self):
         """
         Prueba unitaria que comprueba que la función get_permisos() retorne la lista de
@@ -44,17 +44,18 @@ class TestModelRolProyecto:
         assert rol.nombre != ''
 
 @pytest.mark.django_db
-class TestViewsRolProyecto(unittest.TestCase):
-    def setUp(self):
-        """
-        Setear cliente
-        """
-        self.client = Client()
+class TestViewsRolProyecto(TestCase):
+    def create_rol(self, name="Scrum Master"):
+        return Group.objects.create(name=name)
+
+    def user(self, username="wolfgang", first_name="Wolfgang", last_name="Wiens Wohlgemuth",email="wwwolfgang469@gmail.com"):
+        return User.objects.create(username=username, first_name=first_name,last_name=last_name,email=email)
 
     def test_agregar_rol_proyecto_view(self):
         """
-        Test encargado de comprobar que no ocurra nigun error al cargar la pagina con un usuario que ha iniciado sesion.
+        Test encargado de comprobar que se cargue correctamente la página de agregar rol.
         """
-        response = self.client.get('proyecto/<int:pk_proy>/rol/agregar')
+        rol = self.create_rol()
+        response = self.client.get(reverse('proyecto:agregar-rol',kwargs={'pk':rol.pk}), follow=True)
         self.assertEqual(response.status_code, 200)
  
