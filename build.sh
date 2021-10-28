53abcce469ef7f8e6086958fc9c631a22f46bbb2
Wolfgang Wiens Wohlgemuth <wwwolfgang469@gmail.com>
2021-10-27 21:44:29 -0300

    Change build para desarrollo

--- build.sh
+++ build.sh
@@ -10,64 +10,68 @@ mainmenu () {
     red=`tput setaf 1`
     green=`tput setaf 2`
     reset=`tput sgr0`
+    echo -e "${red}<<< Bienvenidos al Projecto de IS2 del equipo 15 >>>${reset}"
 
     echo "Presione 1 para Desarrollo"
     echo "Presione 2 para Producción"
+    echo "Presione 3 para salir"
+
     read -n 1 -p "Seleccione:" mainmenuinput
     if [ "$mainmenuinput" = "1" ]
     then
         clear
+        echo -e "${green}\n>>> Desarrollo${reset}"
         echo "Presione 1 para TAG v.0.0.1"
         echo "Presione 2 para TAG v.0.0.2"
         echo "Presione 3 para TAG v.0.0.3"
         echo "Presione 4 para TAG v.0.0.4"
         read -n 1 -p "Seleccione el Tag:" tag
 
-        echo -e "\nDesarrollo TAG v.0.0.${tag}:"
+        echo -e "${green}\n>>> Desarrollo TAG v.0.0.${tag}:${reset}"
         echo
-
-        # prepare env and install dependencies
-        echo "${green}>>> Creando entorno virtual${reset}"
-        python3 -m venv env
-        echo "${green}>>> env fue creado.${reset}"
-
-        sleep 2
-        echo "${green}>>> Activando el env.${reset}"
-        source env/bin/activate
-        sleep 2
-
-        echo "${green}>>> Instalando dependencias${reset}"
-        pip install -r requirements.txt
-
-
-        # if [ "$tag" = "1" ]; then
-            # git checkout tags/v.0.0.1 -b v.0.0.1-branch
-        # elif [ "$tag" = "2" ]; then
-        #     # git checkout tags/v.0.0.1 -b v.0.0.1-branch
-        # elif [ "$tag" = "3" ]; then
-        #     # git checkout tags/v.0.0.1 -b v.0.0.1-branch
-        # elif [ "$tag" = "4" ]; then
-        #     # git checkout tags/v.0.0.1 -b v.0.0.1-branch
-        # elif [ "$tag" = "5" ]; then
-        #     # git checkout tags/v.0.0.1 -b v.0.0.1-branch
-        # else 
-        #     echo -e "\nEsa versión no existe"
-        # fi
-
-        # prepare docker and database
-        echo -e "\nIniciando docker"
+         # prepare docker and database
+        echo -e "${green}\n>>> Iniciando docker${reset}"
         . docker-compose_up.sh
         sleep 2
         echo
         sudo docker cp db.dump pg_container:/
-        echo -e "\nCopiando backup a la BD..."
+        echo -e "${green}\n>>> Copiando backup a la BD${reset}"
         sudo docker exec -it pg_container dropdb -U ${db_user} --if-exists ${des_db_name}
         sudo docker exec -it pg_container createdb -U ${db_user} ${des_db_name}
         sudo docker exec -it pg_container psql -U ${db_user} -d ${des_db_name} -c "DROP SCHEMA public CASCADE;"
         sudo docker exec -it pg_container psql -U ${db_user} -d ${des_db_name} -c "CREATE SCHEMA public;"
         sudo docker exec -it pg_container pg_restore -U ${db_user} -d ${des_db_name} --no-owner db.dump
-        echo "Listo para trabajar"
+        echo
 
+        if [ "$tag" = "1" ]; then
+            git checkout tags/v.0.0.1 -b v.0.0.1-branch
+        elif [ "$tag" = "2" ]; then
+            git checkout tags/v.0.0.2 -b v.0.0.2-branch
+        elif [ "$tag" = "3" ]; then
+            git checkout tags/v.0.0.3 -b v.0.0.3-branch
+        elif [ "$tag" = "4" ]; then
+            git switch --detach v.0.0.4
+        elif [ "$tag" = "5" ]; then
+            git checkout tags/v.0.0.5 -b v.0.0.5-branch
+        else 
+            echo -e "${red}\n<<< Esa versión no existe >>>${reset}"
+            sleep 2
+            exit
+        fi
+
+         # prepare env and install dependencies
+        echo -e "${green}\n>>> Creando entorno virtual${reset}"
+        python3 -m venv env
+        echo "${green}>>> env fue creado${reset}"
+        sleep 2
+        echo "${green}>>> Activando el env${reset}"
+        source env/bin/activate
+        sleep 2
+        echo "${green}>>> Instalando dependencias${reset}"
+        pip install -r requirements.txt
+
+        echo -e "${green}\n>>> Listo para trabajar${reset}"
+        echo
         python manage.py runserver
 
     elif [ "$mainmenuinput" = "2" ]; then
@@ -79,30 +83,35 @@ mainmenu () {
         read -n 1 -p "Seleccione el Tag:" tag
         if [ "$tag" = "1" ];
         then
-            echo -e "\nProducción TAG v.0.0.1:"
+            echo -e "${green}\n>>> Producción TAG v.0.0.${tag}:${reset}"
             echo
             
             git checkout production
             git pull
 
             . docker-compose_up.sh
-            read -n 1 -s -r -p "Presione una tecla para continuar..."
+            sleep 2
             echo
 
             sudo docker cp db.dump pg_container:/
-            echo -e "\nCopiando backup a la BD..."
+            echo -e "${green}\n>>> Copiando backup a la BD${reset}"
             sudo docker exec -it pg_container dropdb -U ${db_user} --if-exists ${prod_db_name}
             sudo docker exec -it pg_container createdb -U ${db_user} ${prod_db_name}
             sudo docker exec -it pg_container psql -U ${db_user} -d ${prod_db_name} -c "DROP SCHEMA public CASCADE;"
             sudo docker exec -it pg_container psql -U ${db_user} -d ${prod_db_name} -c "CREATE SCHEMA public;"
             sudo docker exec -it pg_container pg_restore -U ${db_user} -d ${prod_db_name} --no-owner db.dump
-            echo "Listo para trabajar"
+            echo
+            echo "${green}\n>>> Listo para trabajar${reset}"
         fi
+    elif [ "$mainmenuinput" = "3" ]; then
+        echo -e "${red}\n<<< Finalizar >>>${reset}"
+        sleep 2
+        clear
     else
-        echo "You have entered an invallid selection!"
-        echo "Please try again!"
+        echo -e "\nOpción invalida"
+        echo -e "\nPrueba de nuevo!!"
         echo ""
-        echo "Press any key to continue..."
+        echo "Presione una tecla"
         read -n 1
         clear
         mainmenu
@@ -112,6 +121,3 @@ mainmenu () {
 # This builds the main menu and routs the user to the function selected.
 
 mainmenu
-
-# This executes the main menu function.
-# Let the fun begin!!!! WOOT WOOT!!!!
